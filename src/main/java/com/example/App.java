9b9f5aa375af0c725edbd993157a60f019aa1643
navya-tatikonda <tatikondanavya7@gmail.com>
2026-01-21 23:42:14 +0530

    added springboot

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,21 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello CI/CD Pipeline!!!!..");
+        SpringApplication.run(App.class, args);
+    }
+}
+
+@RestController
+class MyController {
+    @GetMapping("/")
+    public String hello() {
+        return "<h1>Hello CI/CD Pipeline!!!!..</h1>";
     }
 }
